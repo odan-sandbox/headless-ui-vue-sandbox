a986f1dceff301a168642ef21642391b27671428
odan <odan3240@gmail.com>
2021-04-28 00:19:26 +0900

    add headlessui

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Menu>
+    <MenuButton>More</MenuButton>
+    <MenuItems>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Account settings
+        </a>
+      </MenuItem>
+      <MenuItem v-slot="{ active }">
+        <a :class='{ "bg-blue-500": active }' href="/account-settings">
+          Documentation
+        </a>
+      </MenuItem>
+      <MenuItem disabled>
+        <span class="opacity-75">Invite a friend (coming soon!)</span>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+      Menu,
+      MenuButton,
+      MenuItems,
+      MenuItem,
+    },
 })
 </script>
 
